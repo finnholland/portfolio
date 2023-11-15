71f9ead2ad907ff25429c5a136ddd3e8fe639c7d
finn <65142636+finnholland@users.noreply.github.com>
2023-11-15 20:21:23 +1030

    better svg

--- app/assets/svgs/sagas.tsx
+++ app/assets/svgs/sagas.tsx
@@ -3,26 +3,17 @@ import { SVGProps } from "react"
 const Sagas = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 92 92"
+    viewBox="0 0 90 90"
     fill="none"
     {...props}
   >
-    <rect width={92} height={92} fill={props["aria-atomic"] ? "#7DD3FC" : "#FFF"} rx={20} />
-    <g clipPath="url(#a)">
-      <path
-        stroke={props["aria-atomic"] ? "#FFF" : "#1B2339"}
-        strokeWidth={4}
-        d="M20 47.5v-15a2.5 2.5 0 0 1 2.5-2.5h15a2.5 2.5 0 0 1 2.5 2.5v15a2.5 2.5 0 0 1-2.5 2.5h-15a2.5 2.5 0 0 1-2.5-2.5Z"
-      />
-      <path stroke={props["aria-atomic"] ? "#FFF" : "#1B2339"} strokeLinecap="round" strokeWidth={8} d="M20 20h52" />
-      <path
-        stroke={props["aria-atomic"] ? "#FFF" : "#1B2339"}
-        strokeWidth={4}
-        d="M48 50h26m0 16H26m0 8h48M48 42h26M48 58h26"
-      />
-      <circle cx={20} cy={66} r={2} fill="#fff" />
-      <circle cx={20} cy={74} r={2} fill="#fff" />
-    </g>
+    <rect width={90} height={90} fill={props["aria-atomic"] ? "#fff" : "#ffffff00"} rx={20} />
+    <path
+      fill={props["aria-atomic"] ? "#7DD3FC" : "#FFF"}
+      fillRule="evenodd"
+      d="M20 0C8.954 0 0 8.954 0 20v50c0 11.046 8.954 20 20 20h50c11.046 0 20-8.954 20-20V20C90 8.954 81.046 0 70 0H20Zm1.565 31.794c0-.247.2-.446.446-.446h14.674c.246 0 .445.2.445.446v14.673c0 .247-.2.446-.445.446H22.01a.446.446 0 0 1-.446-.446V31.794Zm.446-4.446a4.446 4.446 0 0 0-4.446 4.446v14.673a4.446 4.446 0 0 0 4.446 4.446h14.674a4.446 4.446 0 0 0 4.445-4.446V31.794a4.446 4.446 0 0 0-4.445-4.446H22.01Zm-6.446-7.783a4 4 0 0 1 4-4h50.87a4 4 0 0 1 0 8h-50.87a4 4 0 0 1-4-4Zm31.391 23.522h25.435v-4H46.956v4Zm0 7.826h25.435v-4H46.956v4Zm0 7.826h25.435v-4H46.956v4Zm25.435 3.826H25.435v4H72.39v-4ZM25.435 74.391H72.39v-4H25.435v4Zm-3.913-9.826a1.956 1.956 0 1 1-3.913 0 1.956 1.956 0 0 1 3.913 0Zm-1.957 9.783a1.956 1.956 0 1 0 0-3.913 1.956 1.956 0 0 0 0 3.913Z"
+      clipRule="evenodd"
+    />
   </svg>
 )
 export default Sagas
